b8f65363d513880274a4cce753b76b4b1a5368e1
wochae <officialwochae@gmail.com>
2023-11-13 20:09:25 +0900

    refactor: Parser 메시지 반환 및 유효검사 추가
    맵 자료구조에 메뉴와 수량이 제대로 담기는지 그 전에 유효검사는 동작하는지 확인

--- src/main/java/christmas/service/Parser.java
+++ src/main/java/christmas/service/Parser.java
@@ -1,20 +1,29 @@
 package christmas.service;
 
+import static christmas.domain.booking.constants.Constant.AMOUNT_INDEX;
+import static christmas.domain.booking.constants.Constant.FIRST_DAY;
+import static christmas.domain.booking.constants.Constant.LAST_DAY;
+import static christmas.domain.booking.constants.Constant.MENU_AMOUNT_DELIMITER;
+import static christmas.domain.booking.constants.Constant.MENU_INDEX;
+import static christmas.domain.booking.constants.Constant.MENU_TYPE_DELIMITER;
+import static christmas.domain.booking.constants.Constant.SEPARATE_TWO;
 import static christmas.exception.ErrorMessage.DATE_OUT_OF_RANGE;
 import static christmas.exception.ErrorMessage.REQUEST_INVALID_DATE;
+import static christmas.exception.ErrorMessage.REQUEST_INVALID_MENU;
 
-import christmas.exception.DateException;
+import christmas.exception.PlannerException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Parser {
-    private static final int FIRST_DAY = 1;
-    private static final int LAST_DAY = 31;
 
     public static int parseInt(String input) {
         try {
             validateOutOfRange(input);
             return Integer.parseInt(input);
         } catch (NumberFormatException exception) {
-            throw DateException.of(REQUEST_INVALID_DATE, exception);
+            throw PlannerException.of(REQUEST_INVALID_DATE, exception);
         }
     }
 
@@ -23,6 +32,49 @@ public class Parser {
         if (FIRST_DAY <= day && day <= LAST_DAY) {
             return;
         }
-        throw DateException.from(DATE_OUT_OF_RANGE);
+        throw PlannerException.from(DATE_OUT_OF_RANGE);
+    }
+
+    public static Map<String, Integer> splitMenuAndAmount(String input) {
+        Map<String, Integer> menuAndAmountMap = new HashMap<>();
+        try {
+            validateWhiteSpace(input);
+            validateOmittedArgument(input);
+            String[] menuAndAmount = input.split(MENU_TYPE_DELIMITER);
+            Arrays.stream(menuAndAmount)
+                    .forEach(entry -> processMenuEntry(entry, menuAndAmountMap));
+            validateEmpty(menuAndAmountMap);
+        } catch (PlannerException exception) {
+            throw PlannerException.from(REQUEST_INVALID_MENU);
+        }
+        if (menuAndAmountMap.isEmpty()) {
+            throw PlannerException.from(REQUEST_INVALID_MENU);
+        }
+        return menuAndAmountMap;
+    }
+
+    private static void processMenuEntry(String entry, Map<String, Integer> menuAndAmountMap) {
+        String[] parts = entry.split(MENU_AMOUNT_DELIMITER);
+        if (parts.length == SEPARATE_TWO) {
+            menuAndAmountMap.put(parts[MENU_INDEX].trim(), Integer.parseInt(parts[AMOUNT_INDEX].trim()));
+        }
+    }
+
+    private static void validateEmpty(Map<String, Integer> menuAndAmountMap) {
+        if (menuAndAmountMap.isEmpty()) {
+            throw PlannerException.from(REQUEST_INVALID_MENU);
+        }
+    }
+
+    private static void validateOmittedArgument(String input) {
+        if (input.endsWith(MENU_AMOUNT_DELIMITER) || input.endsWith(MENU_TYPE_DELIMITER)) {
+            throw PlannerException.from(REQUEST_INVALID_MENU);
+        }
+    }
+
+    private static void validateWhiteSpace(String input) {
+        if (input.contains(" ")) {
+            throw PlannerException.from(REQUEST_INVALID_MENU);
+        }
     }
 }
--- src/test/java/christmas/service/ParserTest.java
+++ src/test/java/christmas/service/ParserTest.java
@@ -1,7 +1,10 @@
 package christmas.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class ParserTest {
@@ -18,6 +21,36 @@ class ParserTest {
         assertThrows(IllegalArgumentException.class, () -> {
             Parser.parseInt("a");
         });
+    }
+
+    @Test
+    public void null_input() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Parser.parseInt(null);
+        });
+    }
+
+    @Test
+    public void empty_input() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Parser.parseInt("");
+        });
+    }
+
+    @Test
+    public void split_menu_and_amount() {
+        // given
+        Map<String, Integer> expected = new HashMap<>();
+        Map<String, Integer> menuAndAmountMap = new HashMap<>();
+        expected.put("티본스테이크", 1);
+        expected.put("바비큐립", 1);
+        expected.put("초코케이크", 2);
+
+        // when
+        menuAndAmountMap = Parser.splitMenuAndAmount("티본스테이크-1,바비큐립-1,초코케이크-2");
+
+        // then
+        assertThat(menuAndAmountMap).isEqualTo(expected);
     }
 
 }

936c96ff3ceb2c004e56f7b359cc15b2b1bb78d8
wochae <officialwochae@gmail.com>
2023-11-14 14:18:30 +0900

    feat: Payment 할인 전 총 금액, 요일 가져오기
    메뉴의 가격과 수량을 계산한 총량 가져오기, 12월에 몇일이 무슨 요일인지 확인하는 로직 테스트를 통해 확인

---
+++ src/main/java/christmas/service/Payment.java
@@ -0,0 +1,32 @@
+package christmas.service;
+
+import christmas.domain.booking.Booking;
+import christmas.domain.booking.dto.MenuItem;
+import christmas.domain.payment.discount.DayType;
+import java.util.Map;
+
+public class Payment {
+    private final int reservationDay;
+    private final Map<MenuItem, Integer> menuAndAmountMap;
+    private final DayType dayType;
+
+    public Payment(int reservationDay, Map<MenuItem, Integer> menuAndAmountMap) {
+        this.reservationDay = reservationDay;
+        this.menuAndAmountMap = menuAndAmountMap;
+        this.dayType = getDayType(reservationDay);
+    }
+
+    public int getRawTotal(Map<MenuItem, Integer> menuAndAmountMap) {
+        return Booking.getTotalAmount(menuAndAmountMap);
+    }
+
+    public DayType getDayType(int reservationDay) {
+        if (DayType.isWeekday(reservationDay)) {
+            return DayType.WEEKDAY;
+        }
+        if (DayType.isWeekend(reservationDay)) {
+            return DayType.WEEKEND;
+        }
+        return dayType;
+    }
+}
---
+++ src/test/java/christmas/service/PaymentTest.java
@@ -0,0 +1,48 @@
+package christmas.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.domain.booking.dto.MenuItem;
+import christmas.domain.booking.dto.MenuType;
+import christmas.domain.payment.discount.DayType;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class PaymentTest {
+
+    @Test
+    void getRawTotal() {
+        // given
+        int reservationDay = 26;
+        Map<MenuItem, Integer> expected = new HashMap<>();
+        Map<MenuItem, Integer> menuAndAmountMap = new HashMap<>();
+        MenuItem MenuItem1 = new MenuItem(MenuType.MAIN, "티본스테이크", 55_000);
+        MenuItem MenuItem2 = new MenuItem(MenuType.MAIN, "바비큐립", 54_000);
+        MenuItem MenuItem3 = new MenuItem(MenuType.DESSERT, "초코케이크", 15_000);
+        expected.put(MenuItem1, 1);
+        expected.put(MenuItem2, 1);
+        expected.put(MenuItem3, 2);
+        menuAndAmountMap = Parser.splitMenuAndAmount("티본스테이크-1,바비큐립-1,초코케이크-2");
+        Payment payment = new Payment(reservationDay, menuAndAmountMap);
+        // when
+        int expectedTotal = 139_000;
+        int actualTotal = payment.getRawTotal(menuAndAmountMap);
+        // then
+        assertEquals(expectedTotal, actualTotal);
+    }
+
+
+    @Test
+    void getDayType() {
+        // given
+        int reservationDay = 26;
+        Payment payment = new Payment(reservationDay, null);
+        // when
+        DayType expected = DayType.WEEKDAY;
+        DayType actual = payment.getDayType(reservationDay);
+        // then
+        assertEquals(expected, actual);
+
+    }
+}

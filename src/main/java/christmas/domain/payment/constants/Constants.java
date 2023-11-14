ba35ee85303072308af13c04fb883315b526798a
wochae <officialwochae@gmail.com>
2023-11-14 18:30:49 +0900

    feat: print for StringFormat using messageFactory
    현재 주문 건에 대한 모든 할인 및 혜택 메시지 생성 및 출력

--- src/main/java/christmas/domain/payment/constants/Constants.java
+++ src/main/java/christmas/domain/payment/constants/Constants.java
@@ -1,22 +1,16 @@
-package christmas.domain.payment;
+package christmas.domain.payment.constants;
 
 public class Constants {
 
     public static final int BONUS_PRICE = 120_000;
-    public static final int BONUS_YES = 1;
-    public static final int BONUS_NO = 0;
-
+    public static final int BONUS_DISCOUNT = 25_000;
     public static final int AMOUNT_ZERO = 0;
     public static final int FIRST_DAY = 1;
     public static final int SPECIAL_DAY = 25;
-
-    public static final int LAST_DAY = 31;
-
     public static final int BASE_DISCOUNT_AMOUNT = 1000;
     public static final int DAILY_DISCOUNT_INCREASE = 100;
     public static final int WEEKDAY_DESSERT_DISCOUNT = 2023;
     public static final int WEEKEND_MAIN_DISCOUNT = 2023;
-    public static final int SPECIAL_DISCOUNT = 1000;
-
+    public static final int PRESENT_YEAR = 2023;
 
 }
---
+++ src/main/java/christmas/domain/payment/constants/PayMessage.java
@@ -0,0 +1,40 @@
+package christmas.domain.payment.constants;
+
+public enum PayMessage {
+    LOW_PRICE("<증정 메뉴>\n"
+            + "없음\n"
+            + " \n"
+            + "<혜택 내역>\n"
+            + "없음\n"
+            + " \n"
+            + "<총혜택 금액>\n"
+            + "0원\n"
+            + " \n"
+            + "<할인 후 예상 결제 금액>\n"
+            + "%,d원\n"
+            + " \n"
+            + "<12월 이벤트 배지>\n"
+            + "없음"),
+    GIFT_EVENT("\n<증정 메뉴>\n"),
+    ONE_BOTTLE("샴페인 1개\n"),
+    NONE("없음\n"),
+    BENEFIT_RECIPE("\n<혜택 내역>\n"),
+    X_MAX_DISCOUNT("크리스마스 디데이 할인: -%,d원\n"),
+    WEEKDAY_DISCOUNT("평일 할인: -%,d원\n"),
+    WEEKEND_DISCOUNT("주말 할인: -%,d원\n"),
+    SPECIAL_DISCOUNT("특별 할인: -%,d원\n"),
+    GIFT_EVENT_DISCOUNT("증정 이벤트: -%,d원\n"),
+    AMOUNT_BENEFIT_PRICE("\n<총혜택 금액>\n"),
+    EXPECTED_PRICE("\n<할인 후 예상 결제 금액>\n"),
+    PRICE_MINUS_FORMAT("-%,d원\n"),
+    PRICE_FORMAT("%,d원\n"),
+    BADGE("\n<12월 이벤트 배지>\n"),
+    SANTA("산타");
+    private final String message;
+    PayMessage(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+}
